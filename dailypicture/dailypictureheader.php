2aae835c8097c5aeb8c59c702dc8bc81634ca065
brianleemcqueen <brian.lee.mcqueen@gmail.com>
2020-10-28 23:57:13 -0400

    dailypicture

---
+++ dailypicture/dailypictureheader.php
@@ -0,0 +1,36 @@
+<html>
+
+<head>
+<title>mcqueentree presents the Daily Picture</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<link rel="STYLESHEET" href="../../css/main_style.css" TYPE="text/css">
+</head>
+
+<body bgcolor="#ffffff">
+<div id="header" class="imagediv" style="width: 166; height: 39">
+<a href="../../../index.html">
+<img border="0" src="../../images/mcqueentree/tree-t.gif"></a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+</div>
+
+    
+
+
+<div id="content" style="width: 556; height: 27">
+	
+	<table border="0" cellpadding="0" cellspacing="0" width="686" id="headertable">
+      <tr>
+        <td width="425"><font color="#996633" size="2">The official site of the McQueen Family</font></td>
+        <td width="261">
+        	<a href="../../rss.xml">
+   			<img border="0" src="../../images/xmlrss.jpg" align="right">
+   		</a>
+   		<a href="http://add.my.yahoo.com/rss?url=http://www.mcqueentree.com">
+ 			<img align="right" border=0 src="http://us.i1.yimg.com/us.yimg.com/i/us/my/addtomyyahoo2.gif" width="100" height="14">
+		</a>
+	</td>
+      </tr>
+    </table>
+</div>
+
+<div id="navigation">
+	<span class="header16Green"><font size="3">The Daily Picture</font></span>
